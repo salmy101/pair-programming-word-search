c46a2d9e9cafc69ea7438dc6161203987f5c0e9b
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-06-08 22:48:12 +0000

    paired programming with Mr.Moon!

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,49 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
+  let result = [];
+  let colLength = matrix[0].length;
+  let rowLength = matrix.length;
 
-module.exports = wordSearch
+  for (let row = 0; row < colLength; row++) {
+    if (colLength > 1) {
+      result[row] = [];
+    }
+    for (let col = 0; col < rowLength; col++) {
+      result[row][col] = matrix[col][row];
+    }
+  }
+
+  return result;
+};
+
+
+//function recieves a 2D array of letters and words => Check to find word horizontally & vertically => return TRUE if word
+// IS FOUND, return FALSE if word is NOT FOUND.
+
+const wordSearch = (letters, word) => {
+
+  if (typeof(word) !== "string") {
+    return false;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  //console.log('horizontalJoin', horizontalJoin)
+  for (let l of horizontalJoin) {
+    //console.log("l:", l);
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  const lettersFlipped = transpose(letters);
+  const verticalJoin = lettersFlipped.map(ls => ls.join(''));
+  //console.log('verticalJoin', verticalJoin)
+  for (let j of verticalJoin) {
+    // console.log("j:", j);
+    if (j.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+
+};
+
+
+module.exports = wordSearch;
